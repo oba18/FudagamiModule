f1a13f4bbbc0885f96e2af5cc699ace05fb03184
oba18 <m.oba.0426@gmail.com>
2020-10-27 14:50:11 +0900

    mod

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
+import numpy as np
+
 import FudagamiModule as fm
-fm.hello()
+src = '../team_OK/mps/experiment_videos/094448.mp4'
+#  src = np.array([0,0,0,0])
+#  src = [0,0,0,0]
+
+video = fm.new(src)
---
+++ video.py
@@ -0,0 +1,3 @@
+class Video:
+    def __init__(self, src):
+        self.src = src
